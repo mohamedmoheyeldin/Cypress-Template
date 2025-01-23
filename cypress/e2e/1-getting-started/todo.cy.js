5da65dc67ea1d4db1da7ac8b68d8bf71582cbe4a
Mohamed Moheyeldin <mohamedrmoheyeldin@gmail.com>
2025-01-23 16:36:45 -0500

    Update commands.js

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -12,13 +12,7 @@
 // https://on.cypress.io/introduction-to-cypress
 
 describe('example to-do app', () => {
-  beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
-    cy.visit('https://example.cypress.io/todo')
-  })
+
 
   it('displays two todo items by default', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,12 +24,143 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-/*
 beforeEach(() => {
     cy.visit('/')
     cy.request("/").then((response) => {
         expect(response.status).to.eq(200)
     })
-    cy.title().should('eq', '')
-    cy.injectAxe()
-})*/
+    cy.title().should('eq', 'Google')
+})
+
+
+// Click link containing text
+// Usage  cy.clickLink('Buy Now')
+Cypress.Commands.add('clickLink', (label) => {
+    cy.get('a').contains(label).click()
+})
+
+
+// Check a token
+// Usage  cy.checkToken('abc123')
+Cypress.Commands.add('checkToken', (token) => {
+    cy.window().its('localStorage.token').should('eq', token)
+})
+
+
+// Download a file
+// Usage  cy.downloadFile('https://path_to_file.pdf', 'mydownloads', 'demo.pdf')
+Cypress.Commands.add('downloadFile', (url, directory, fileName) => {
+    return cy.getCookies().then((cookies) => {
+        return cy.task('downloadFile', {
+            url,
+            directory,
+            cookies,
+            fileName,
+        })
+    })
+})
+
+
+// Commands to work with sessionStorage
+// Usage  cy.setSessionStorage('token', 'abc123')
+// Usage  cy.getSessionStorage('token').should('eq', 'abc123')
+Cypress.Commands.add('getSessionStorage', (key) => {
+    cy.window().then((window) => window.sessionStorage.getItem(key))
+})
+
+Cypress.Commands.add('setSessionStorage', (key, value) => {
+    cy.window().then((window) => {
+        window.sessionStorage.setItem(key, value)
+    })
+})
+
+
+// Log in command using UI
+// Usage  cy.loginViaUi({ email: 'fake@email.com', password: '$ecret1', name: 'johndoe' })
+Cypress.Commands.add('loginViaUi', (user) => {
+    cy.session(
+        user,
+        () => {
+            cy.visit('/login')
+            cy.get('input[name=email]').type(user.email)
+            cy.get('input[name=password]').type(user.password)
+            cy.click('button#login')
+            cy.get('h1').contains(`Welcome back ${user.name}!`)
+        },
+        {
+            validate: () => {
+                cy.getCookie('auth_key').should('exist')
+            },
+        }
+    )
+})
+
+
+// Log in command using request
+// can start a chain off of cy
+cy.loginViaApi('admin')
+
+// can be chained but will not receive the previous subject
+cy.get('button').loginViaApi('user')
+Cypress.Commands.add('loginViaApi', (userType, options = {}) => {
+    // this is an example of skipping your UI and logging in programmatically
+
+    // setup some basic types
+    // and user properties
+    const types = {
+        admin: {
+            name: 'Jane Lane',
+            admin: true,
+        },
+        user: {
+            name: 'Jim Bob',
+            admin: false,
+        },
+    }
+
+    // grab the user
+    const user = types[userType]
+
+    // create the user first in the DB
+    cy.request({
+        url: '/seed/users', // assuming you've exposed a seeds route
+        method: 'POST',
+        body: user,
+    })
+        .its('body')
+        .then((body) => {
+            // assuming the server sends back the user details
+            // including a randomly generated password
+            //
+            // we can now login as this newly created user
+            cy.request({
+                url: '/login',
+                method: 'POST',
+                body: {
+                    email: body.email,
+                    password: body.password,
+                },
+            })
+        })
+})
+
+
+// Log out command using UI
+// Usage  cy.logout()
+Cypress.Commands.add('logout', () => {
+    cy.contains('Login').should('not.exist')
+    cy.get('.avatar').click()
+    cy.contains('Logout').click()
+    cy.get('h1').contains('Login')
+    cy.getCookie('auth_key').should('not.exist')
+})
+
+
+// Log out command using localStorage End-to-End Only
+// Usage  cy.logout()
+Cypress.Commands.add('logout', () => {
+    cy.window().its('localStorage').invoke('removeItem', 'session')
+
+    cy.visit('/login')
+})
+
