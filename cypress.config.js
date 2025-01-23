2654560d60221a2764f4d3964a8527a27f071bc0
Mohamed Moheyeldin <mohamedrmoheyeldin@gmail.com>
2025-01-23 13:51:51 -0500

    Update cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const {defineConfig} = require("cypress");
 module.exports = defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
-            require('cypress-mochawesome-reporter/plugin')(on);
         },
         baseUrl: 'https://www.google.com',
         viewportHeight: 1080,
